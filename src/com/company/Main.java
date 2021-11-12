6b2efd6ac042b127ebd86142ad8ac3ff0392a6f1
red312 <orman.213@yandex.ru>
2021-11-12 20:13:52 +0300

    push2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,8 @@ import static com.company.Encoder.Encoder.encodeFile;
 
 class Main{
     public static void main(String[] args) throws IOException {
-//        for (String arg: args){
-//            encodeFile(arg);
-//        }
-        encodeFile("/home/roman/Documents/пары/data for zip/pic");
+        for (String arg: args){
+            encodeFile(arg);
+        }
     }
 }
